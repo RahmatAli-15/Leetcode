2e8880340db646366a9d107485352a09658e7a84
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-03-27 13:22:22 +0530

    Create Leetcode-713.java

---
+++ Leetcode-713.java
@@ -0,0 +1,16 @@
+713. Subarray Product Less Than K
+
+  class Solution {
+    public int numSubarrayProductLessThanK(int[] nums, int k) {
+        
+   int ans = 0;
+        for (int i = 0, j = 0, s = 1; i < nums.length; ++i) {
+            s *= nums[i];
+            while (j <= i && s >= k) {
+                s /= nums[j++];
+            }
+            ans += i - j + 1;
+        }
+        return ans;
+    }
+}
