f2cefc8f448ac19d5a9a4dcfe31cd66e4b855d6f
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-04-03 23:28:10 +0530

    Update Leetcode-79.java

--- Leetcode-79.java
+++ Leetcode-79.java
@@ -1,5 +1,7 @@
 79. Word Search
 
+  class Solution {
+    public boolean exist(char[][] board, String word) {
   for (int i = 0; i < board.length; ++i)
       for (int j = 0; j < board[0].length; ++j)
         if (dfs(board, word, i, j, 0))
