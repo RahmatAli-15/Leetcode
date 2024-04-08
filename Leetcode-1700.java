440cdaaa21ce349d585baaef376a59c14d0e0d20
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-04-08 17:02:09 +0530

    Create Leetcode-1700.java

---
+++ Leetcode-1700.java
@@ -0,0 +1,19 @@
+1700. Number of Students Unable to Eat Lunch
+
+  class Solution {
+    public int countStudents(int[] students, int[] sandwiches) {
+        
+   int[] count = new int[2];
+
+    for (final int student : students)
+      ++count[student];
+
+    for (int i = 0; i < sandwiches.length; ++i) {
+      if (count[sandwiches[i]] == 0)
+        return sandwiches.length - i;
+      --count[sandwiches[i]];
+    }
+
+    return 0;
+  }
+}
