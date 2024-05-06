701456dac4c5ac8ced282c9b533ff29e10abe243
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-05-06 22:24:52 +0530

    Create Leetcode-2487.java

---
+++ Leetcode-2487.java
@@ -0,0 +1,10 @@
+2487. Remove Nodes From Linked List
+
+  class Solution {
+  public ListNode removeNodes(ListNode head) {
+    if (head == null)
+      return null;
+    head.next = removeNodes(head.next);
+    return head.next != null && head.val < head.next.val ? head.next : head;
+  }
+}
