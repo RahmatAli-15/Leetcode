c5dfcfb8f70f0e14134489bbdbe07d4769778dcc
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-06-17 18:06:37 +0530

    Create Leetcode-633.java

---
+++ Leetcode-633.java
@@ -0,0 +1,20 @@
+633. Sum of Square Numbers
+
+  class Solution {
+  public boolean judgeSquareSum(int c) {
+    int l = 0;
+    int r = (int) Math.sqrt(c);
+
+    while (l <= r) {
+      final int sum = l * l + r * r;
+      if (sum == c)
+        return true;
+      if (sum < c)
+        ++l;
+      else
+        --r;
+    }
+
+    return false;
+  }
+}
