82a02b48234f815cfc002ba4ce131a898400ba0a
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-08-03 19:26:54 +0530

    Create Leetcode-1460.java

---
+++ Leetcode-1460.java
@@ -0,0 +1,12 @@
+1460. Make Two Arrays Equal by Reversing Subarrays
+
+
+  class Solution {
+  public boolean canBeEqual(int[] target, int[] arr) {
+    return Arrays.stream(arr)
+        .boxed()
+        .collect(Collectors.groupingBy(Integer::intValue, Collectors.counting()))
+        .equals(Arrays.stream(target).boxed().collect(
+            Collectors.groupingBy(Integer::intValue, Collectors.counting())));
+  }
+}
