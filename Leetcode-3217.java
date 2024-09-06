9e33807299140e4f33bd33341697eb4ed74f4eb2
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-09-06 19:36:18 +0530

    Create Leetcode-3217.java

---
+++ Leetcode-3217.java
@@ -0,0 +1,19 @@
+3217. Delete Nodes From Linked List Present in Array
+
+class Solution {
+    public ListNode modifiedList(int[] nums, ListNode head) {
+        Set<Integer> s = new HashSet<>();
+        for (int x : nums) {
+            s.add(x);
+        }
+        ListNode dummy = new ListNode(0, head);
+        for (ListNode pre = dummy; pre.next != null;) {
+            if (s.contains(pre.next.val)) {
+                pre.next = pre.next.next;
+            } else {
+                pre = pre.next;
+            }
+        }
+        return dummy.next;
+    }
+}
