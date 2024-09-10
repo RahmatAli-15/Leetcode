2efb78f6431765e0cfca1dfaa41a9c57d0ff9383
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-09-10 19:10:50 +0530

    Create Leetcode-2807.java

---
+++ Leetcode-2807.java
@@ -0,0 +1,17 @@
+2807. Insert Greatest Common Divisors in Linked List
+
+
+  class Solution {
+  public ListNode insertGreatestCommonDivisors(ListNode head) {
+    for (ListNode curr = head; curr.next != null;) {
+      ListNode inserted = new ListNode(gcd(curr.val, curr.next.val), curr.next);
+      curr.next = inserted;
+      curr = inserted.next;
+    }
+    return head;
+  }
+
+  private int gcd(int a, int b) {
+    return b == 0 ? a : gcd(b, a % b);
+  }
+}
