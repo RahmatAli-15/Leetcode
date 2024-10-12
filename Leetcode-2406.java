4b6861b30526aae222680c50e311d66206fc32f7
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-10-12 18:49:15 +0530

    Create Leetcode-2406.java

---
+++ Leetcode-2406.java
@@ -0,0 +1,20 @@
+2406. Divide Intervals Into Minimum Number of Groups
+
+  class Solution {
+  // Similar to 253. Meeting Rooms II
+  public int minGroups(int[][] intervals) {
+    // Stores `right`s.
+    Queue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(a, b));
+
+    Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+    for (int[] interval : intervals) {
+      // There's no overlap, so we can reuse the same group.
+      if (!minHeap.isEmpty() && interval[0] > minHeap.peek())
+        minHeap.poll();
+      minHeap.offer(interval[1]);
+    }
+
+    return minHeap.size();
+  }
+}
