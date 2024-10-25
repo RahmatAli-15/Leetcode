f7408b0909b203fe4aa10d380d60eab8920d997f
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-10-25 18:52:07 +0530

    Create Leetcode-1233.java

---
+++ Leetcode-1233.java
@@ -0,0 +1,19 @@
+1233. Remove Sub-Folders from the Filesystem
+
+  class Solution {
+  public List<String> removeSubfolders(String[] folder) {
+    List<String> ans = new ArrayList<>();
+    String prev = "";
+
+    Arrays.sort(folder);
+
+    for (final String f : folder) {
+      if (!prev.isEmpty() && f.startsWith(prev) && f.charAt(prev.length()) == '/')
+        continue;
+      ans.add(f);
+      prev = f;
+    }
+
+    return ans;
+  }
+}
