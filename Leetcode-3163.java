8bdf5266c88c3e61b07c4c1a786f270bb18ca05c
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-11-04 20:58:57 +0530

    Create Leetcode-3163.java

---
+++ Leetcode-3163.java
@@ -0,0 +1,19 @@
+3163. String Compression III
+
+  class Solution {
+  public String compressedString(String word) {
+    final int n = word.length();
+    StringBuilder sb = new StringBuilder();
+
+    for (int i = 0, j = 0; i < n; i = j) {
+      int count = 0;
+      while (j < n && word.charAt(j) == word.charAt(i) && count < 9) {
+        ++j;
+        ++count;
+      }
+      sb.append(String.valueOf(count)).append(word.charAt(i));
+    }
+
+    return sb.toString();
+  }
+}
