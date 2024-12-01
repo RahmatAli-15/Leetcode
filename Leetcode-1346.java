f63153b7212638a2f1335952cb7791d336456fcf
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-01 12:27:32 +0530

    Create Leetcode-1346.java

---
+++ Leetcode-1346.java
@@ -0,0 +1,16 @@
+1346. Check If N and Its Double Exist
+
+
+  class Solution {
+  public boolean checkIfExist(int[] arr) {
+    Set<Integer> seen = new HashSet<>();
+
+    for (final int a : arr) {
+      if (seen.contains(a * 2) || a % 2 == 0 && seen.contains(a / 2))
+        return true;
+      seen.add(a);
+    }
+
+    return false;
+  }
+}
