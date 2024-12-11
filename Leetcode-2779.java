0d4d00e98feebde9acd823589899470f3d2ee022
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-11 21:50:21 +0530

    Create Leetcode-2779.java

---
+++ Leetcode-2779.java
@@ -0,0 +1,17 @@
+2779. Maximum Beauty of an Array After Applying Operation
+
+  class Solution {
+  public int maximumBeauty(int[] nums, int k) {
+    int ans = 0;
+
+    Arrays.sort(nums);
+
+    for (int l = 0, r = 0; r < nums.length; ++r) {
+      while (nums[r] - nums[l] > 2 * k)
+        ++l;
+      ans = Math.max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+}
