66c90efb7ae6033cdaf2eb308cadac269734dd30
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-12 20:04:26 +0530

    Create Leetcode-2558.java

---
+++ Leetcode-2558.java
@@ -0,0 +1,21 @@
+2558. Take Gifts From the Richest Pile
+
+  class Solution {
+  public long pickGifts(int[] gifts, int k) {
+    long ans = 0;
+    Queue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+
+    for (final int gift : gifts)
+      maxHeap.offer(gift);
+
+    for (int i = 0; i < k; ++i) {
+      final int squaredMax = (int) Math.sqrt(maxHeap.poll());
+      maxHeap.offer(squaredMax);
+    }
+
+    while (!maxHeap.isEmpty())
+      ans += maxHeap.poll();
+
+    return ans;
+  }
+}
