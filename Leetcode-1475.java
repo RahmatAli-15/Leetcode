8975fda36ee1cfd0d18edec3b6e6d0e6259617c3
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-18 22:07:14 +0530

    Create Leetcode-1475.java

---
+++ Leetcode-1475.java
@@ -0,0 +1,17 @@
+1475. Final Prices With a Special Discount in a Shop
+
+  class Solution {
+  public int[] finalPrices(int[] prices) {
+    int[] ans = prices.clone();
+    Deque<Integer> stack = new ArrayDeque<>();
+
+    for (int j = 0; j < prices.length; ++j) {
+      // stack[-1] := i in the problem description.
+      while (!stack.isEmpty() && prices[j] <= prices[stack.peek()])
+        ans[stack.pop()] -= prices[j];
+      stack.push(j);
+    }
+
+    return ans;
+  }
+}
