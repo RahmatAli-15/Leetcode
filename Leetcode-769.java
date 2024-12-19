3264c980ba9d64992ad819e362b905fba69a1475
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-19 21:36:19 +0530

    Create Leetcode-769.java

---
+++ Leetcode-769.java
@@ -0,0 +1,17 @@
+769. Max Chunks To Make Sorted
+
+
+  class Solution {
+  public int maxChunksToSorted(int[] arr) {
+    int ans = 0;
+    int mx = Integer.MIN_VALUE;
+
+    for (int i = 0; i < arr.length; ++i) {
+      mx = Math.max(mx, arr[i]);
+      if (mx == i)
+        ++ans;
+    }
+
+    return ans;
+  }
+}
