b75bcf0781c2c5d2d38827d8e69b57aa8d3f9cbf
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2024-12-27 20:45:20 +0530

    Create Leetcode-1014.java

---
+++ Leetcode-1014.java
@@ -0,0 +1,15 @@
+1014. Best Sightseeing Pair
+  
+  class Solution {
+  public int maxScoreSightseeingPair(int[] values) {
+    int ans = 0;
+    int bestPrev = 0;
+
+    for (final int value : values) {
+      ans = Math.max(ans, value + bestPrev);
+      bestPrev = Math.max(bestPrev, value) - 1;
+    }
+
+    return ans;
+  }
+}
