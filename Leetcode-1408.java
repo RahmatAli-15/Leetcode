0f14bb513ceb2a3b4086b2803fb91c7de7137946
Rahmat Ali <147299931+RahmatAli-15@users.noreply.github.com>
2025-01-07 09:20:37 +0530

    Create Leetcode-1408.java

---
+++ Leetcode-1408.java
@@ -0,0 +1,14 @@
+1408. String Matching in an Array
+
+  class Solution {
+  public List<String> stringMatching(String[] words) {
+    List<String> ans = new ArrayList<>();
+    for (final String a : words)
+      for (final String b : words)
+        if (a.length() < b.length() && b.indexOf(a) != -1) {
+          ans.add(a);
+          break;
+        }
+    return ans;
+  }
+}
